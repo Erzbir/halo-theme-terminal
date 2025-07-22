70b5dc7a7fd96e3f3f974d6e1bf8d6bb11294605
Erzbir <erzbir@mail.com>
2025-07-22 15:50:49 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,18 @@
+# Halo Terminal
+
+一款 Terminal 风格的 Halo 主题
+
+基于 wan92hen 的 [Terminal](https://github.com/wan92hen/theme-terminal)
+修改, 使得更接近原版 [Hugo Terminal](https://github.com/panr/hugo-theme-terminal)
+
+![demo](assets/images/demo.png)
+
+## 特性
+- 明暗模式
+- 首页公告
+- 评论
+- 友链
+- 错误页
+
+## 原主题
+- [Terminal](https://github.com/wan92hen/theme-terminal)
---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import "./styles/style.scss";
+import "./styles/fonts.scss";
+import "./styles/font-pixel.scss";
+import Alpine from "alpinejs";
+import upvote from "./upvote";
+// @ts-ignore
+import tocbot from "tocbot";
+
+window.Alpine = Alpine;
+
+// @ts-ignore
+Alpine.data("upvote", upvote);
+
+Alpine.start();
+
+export function generateToc() {
+  const content = document.getElementById("content");
+  const titles = content?.querySelectorAll("h1, h2, h3, h4");
+  if (!titles || titles.length === 0) {
+    const tocContainer = document.querySelector(".toc-container");
+    tocContainer?.remove();
+    return;
+  }
+  // @ts-ignore
+  tocbot.init({
+    tocSelector: ".toc",
+    contentSelector: "#content",
+    headingSelector: "h1, h2, h3, h4",
+    extraListClasses: "toc-list",
+    extraLinkClasses: "toc-link",
+    headingsOffset: 96,
+    scrollSmoothOffset: -96,
+    scrollSmooth: true,
+    // @ts-ignore
+    // tocScrollOffset: 50,
+  });
+}
+
+export function locateToc() {
+  const content = document.getElementById("content");
+  const toc = document.querySelector(".toc") as HTMLElement;
+  const contentRight = content?.getBoundingClientRect().right;
+  if (toc && contentRight) {
+    toc.style.left = `${contentRight}px`;
+  }
+}
